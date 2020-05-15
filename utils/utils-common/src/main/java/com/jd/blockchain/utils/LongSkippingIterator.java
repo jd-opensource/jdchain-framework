dd984d7545a12d31dc4c13013a7956cde55e8f7f
huanghaiquan <huanghaiquan@jd.com>
2020-05-15 14:00:35 +0800

    added util : EmptyLongSkippingIterator;

--- utils/utils-common/src/main/java/com/jd/blockchain/utils/LongSkippingIterator.java
+++ utils/utils-common/src/main/java/com/jd/blockchain/utils/LongSkippingIterator.java
@@ -2,12 +2,17 @@ package com.jd.blockchain.utils;
 
 import java.util.Iterator;
 
-public interface LongSkippingIterator<E> extends Iterator<E> {
 
+public interface LongSkippingIterator<E> extends Iterator<E> {
+	
 	long getCount();
 
 	long getCursor();
 
-	long skip(long count);
+	long skip(long skippingCount);
 
+	
+	public static <E> LongSkippingIterator<E> empty(){
+		return new EmptyLongSkippingIterator<E>();
+	}
 }
