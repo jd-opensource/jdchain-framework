aae6183486b675a33b69c9e5b84af7052d633dc9
huanghaiquan <huanghaiquan@jd.com>
2020-06-08 11:01:13 +0800

    fixed compiling errors;

--- ledger-model/src/main/java/com/jd/blockchain/ledger/LedgerInitProperties.java
+++ ledger-model/src/main/java/com/jd/blockchain/ledger/LedgerInitProperties.java
@@ -7,6 +7,7 @@ import java.io.Serializable;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
