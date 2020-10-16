b761357f50370fef31ea922920f2d27fb7f6a666
zhangshuang <zhangshuang3@jd.com>
2020-10-16 10:43:20 +0800

    fix gateway query transactions proxyobj error

--- utils/utils-web/src/main/java/com/jd/blockchain/utils/web/model/JsonWebResponseMessageConverter.java
+++ utils/utils-web/src/main/java/com/jd/blockchain/utils/web/model/JsonWebResponseMessageConverter.java
@@ -33,39 +33,36 @@ public class JsonWebResponseMessageConverter extends FastJsonHttpMessageConverte
 		if (obj instanceof WebResponse) {
 			super.writeInternal(obj, outputMessage);
 			return;
+		} else if (obj.getClass().isArray()) {
+			// 数组类型需要判断是否为代理对象
+			Object[] objects = (Object[]) obj;
+			if (objects != null && objects.length > 0) {
+				Object[] results = new Object[objects.length];
+				for (int i = 0; i < objects.length; i++) {
+					results[i] = objects[i];
+					if (objects[i] instanceof Proxy) {
+						try {
+							results[i] = getProxyHandler(objects[i]);
+						} catch (Exception e) {
+							super.writeInternal(WebResponse.createSuccessResult(obj), outputMessage);
+							return;
+						}
+					}
+				}
+				super.writeInternal(WebResponse.createSuccessResult(results), outputMessage);
+				return;
+			}
+		} else if (obj instanceof Proxy) {
+			try {
+				Object result = getProxyHandler(obj); // 获取Proxy对象进行转换
+				super.writeInternal(WebResponse.createSuccessResult(result), outputMessage);
+				return;
+			} catch (Exception e) {
+				super.writeInternal(WebResponse.createSuccessResult(obj), outputMessage);
+				return;
+			}
 		}
 		super.writeInternal(WebResponse.createSuccessResult(obj), outputMessage);
-		
-//		} else if (obj.getClass().isArray()) {
-//			// 数组类型需要判断是否为代理对象
-//			Object[] objects = (Object[]) obj;
-//			if (objects != null && objects.length > 0) {
-//				Object[] results = new Object[objects.length];
-//				for (int i = 0; i < objects.length; i++) {
-//					results[i] = objects[i];
-//					if (objects[i] instanceof Proxy) {
-//						try {
-//							results[i] = getProxyHandler(objects[i]);
-//						} catch (Exception e) {
-//							super.writeInternal(WebResponse.createSuccessResult(obj), outputMessage);
-//							return;
-//						}
-//					}
-//				}
-//				super.writeInternal(WebResponse.createSuccessResult(results), outputMessage);
-//				return;
-//			}
-//		} else if (obj instanceof Proxy) {
-//			try {
-//				Object result = getProxyHandler(obj); // 获取Proxy对象进行转换
-//				super.writeInternal(WebResponse.createSuccessResult(result), outputMessage);
-//				return;
-//			} catch (Exception e) {
-//				super.writeInternal(WebResponse.createSuccessResult(obj), outputMessage);
-//				return;
-//			}
-//		}
-//		super.writeInternal(WebResponse.createSuccessResult(obj), outputMessage);
 	}
 
 	/**
