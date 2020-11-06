691756db48eb5b768cdcb8dd895aaab6de93c16e
huanghaiquan <huanghaiquan@jd.com>
2020-11-06 23:19:32 +0800

    optimized skipping iterator;

--- utils/utils-common/src/main/java/com/jd/blockchain/utils/SkippingIterator.java
+++ utils/utils-common/src/main/java/com/jd/blockchain/utils/SkippingIterator.java
@@ -1,5 +1,6 @@
 package com.jd.blockchain.utils;
 
+import java.lang.reflect.Array;
 import java.util.Iterator;
 
 /**
@@ -17,18 +18,21 @@ public interface SkippingIterator<E> extends Iterator<E> {
 	 * @return
 	 */
 	long getTotalCount();
-	
+
 	/**
-	 * 迭代器游标，指示了当前迭代器的进度；<p>
+	 * 迭代器游标，指示了当前迭代器的进度；
+	 * <p>
 	 * 
-	 * 游标的初始值为 -1，当迭代器到达末尾（即 {@link #hasNext()} 为 false）时，游标的最大值为（{@link #getTotalCount()}-1）；
+	 * 游标的初始值为 -1，当迭代器到达末尾（即 {@link #hasNext()} 为
+	 * false）时，游标的最大值为（{@link #getTotalCount()}-1）；
 	 * 
 	 * @return
 	 */
 	long getCursor();
-	
+
 	/**
 	 * 剩余的数量；
+	 * 
 	 * @return
 	 */
 	default long getCount() {
@@ -42,5 +46,67 @@ public interface SkippingIterator<E> extends Iterator<E> {
 	 * @return 返回实际略过的个数；如果剩余的数量少于要略过的数量，则返回剩余的数量；
 	 */
 	long skip(long count);
+
+	/**
+	 * 连续返回指定数量的元素；
+	 * 
+	 * @param maxCount
+	 * @param clazz
+	 * @return
+	 */
+	default E[] next(int maxCount, Class<E> clazz) {
+		int count = (int) Math.min(getCount(), (long) maxCount);
+		@SuppressWarnings("unchecked")
+		E[] datas = (E[]) Array.newInstance(clazz, count);
+
+		next(datas, 0, count);
+
+		return datas;
+	}
+
+	/**
+	 * 连续返回指定数量的元素；
+	 * <p>
+	 * 
+	 * 从位置 0 开始记录输出元素，直到填满数组的全部空间；
+	 * 
+	 * @param datas 输出元素的数组；
+	 * @return 实际输出的元素的数量；
+	 */
+	default int next(E[] datas) {
+		return next(datas, 0, datas.length);
+	}
+
+	/**
+	 * 连续返回指定数量的元素；
+	 * <p>
+	 * 
+	 * 从指定位置开始记录输出元素，直到填满数组的全部空间；
+	 * 
+	 * @param datas  输出元素的数组；
+	 * @param offset 在数组中记录输出元素的起始偏移位置；
+	 * @return 实际输出的元素的数量；
+	 */
+	default int next(E[] datas, int offset) {
+		return next(datas, 0, datas.length);
+	}
+
+	/**
+	 * 连续返回指定数量的元素；
+	 * 
+	 * @param datas    输出元素的数组；
+	 * @param offset   在数组中记录输出元素的起始偏移位置；
+	 * @param maxCount 最大数量；
+	 * @return 实际输出的元素的数量；
+	 */
+	default int next(E[] datas, int offset, int maxCount) {
+		int count = (int) Math.min(getCount(), (long) maxCount);
+		int i = 0;
+		while (i < count && hasNext()) {
+			datas[i] = (E) next();
+			i++;
+		}
+		return count;
+	}
 
 }
