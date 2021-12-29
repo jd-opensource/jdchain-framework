c2d42c6717c7ba3a9df4e38d8a423bea03acfc65
liuyuanmu <liuyuanmu@jd.com>
2021-12-29 09:25:10 +0800

    add prometheus for peer

--- ledger-model/src/main/java/com/jd/blockchain/service/TransactionEngine.java
+++ ledger-model/src/main/java/com/jd/blockchain/service/TransactionEngine.java
@@ -1,11 +1,14 @@
 package com.jd.blockchain.service;
 
 import com.jd.blockchain.crypto.HashDigest;
+import com.jd.blockchain.metrics.LedgerMetrics;
 
 public interface TransactionEngine {
 	
 	TransactionBatchProcess createNextBatch(HashDigest ledgerHash);
-	
+
+	TransactionBatchProcess createNextBatch(HashDigest ledgerHash, LedgerMetrics metrics);
+
 	TransactionBatchProcess getBatch(HashDigest ledgerHash);
 	
 }
